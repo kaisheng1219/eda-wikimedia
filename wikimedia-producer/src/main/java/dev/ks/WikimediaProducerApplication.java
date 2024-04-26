73a24ec549af3ae9133d40a6e2e9b68ef0f42d08
Kai Sheng <76138041+kaisheng1219@users.noreply.github.com>
2024-04-26 18:30:06 +0800

    :star: new: runs wikimedia tracking on start

--- wikimedia-producer/src/main/java/dev/ks/WikimediaProducerApplication.java
+++ wikimedia-producer/src/main/java/dev/ks/WikimediaProducerApplication.java
@@ -1,12 +1,25 @@
 package dev.ks;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WikimediaProducerApplication {
-    public static void main( String[] args )
+public class WikimediaProducerApplication implements CommandLineRunner {
+    private final WikimediaChangesProducer wikimediaChangesProducer;
+
+    public WikimediaProducerApplication(WikimediaChangesProducer wikimediaChangesProducer) {
+        this.wikimediaChangesProducer = wikimediaChangesProducer;
+    }
+
+    public static void main(String[] args )
     {
         SpringApplication.run(WikimediaProducerApplication.class);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
     }
 }
