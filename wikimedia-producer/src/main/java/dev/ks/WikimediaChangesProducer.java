93b22adb667a7b6774951781894fa6778932f1d4
Kai Sheng <76138041+kaisheng1219@users.noreply.github.com>
2024-04-26 18:29:06 +0800

    :star: new: consume wikimedia recentchange url to produce msg

---
+++ wikimedia-producer/src/main/java/dev/ks/WikimediaChangesProducer.java
@@ -0,0 +1,33 @@
+package dev.ks;
+
+import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.background.BackgroundEventHandler;
+import com.launchdarkly.eventsource.background.BackgroundEventSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class WikimediaChangesProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WikimediaChangesProducer.class);
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public WikimediaChangesProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage() throws InterruptedException {
+        String topic = "wikimedia_recent_change";
+        BackgroundEventHandler eventHandler = new WikimediaChangesHandler(kafkaTemplate, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(URI.create(url));
+        BackgroundEventSource.Builder eventSource = new BackgroundEventSource.Builder(eventHandler, builder);
+        BackgroundEventSource source = eventSource.build();
+        source.start();
+        TimeUnit.MINUTES.sleep(10);
+    }
+}
