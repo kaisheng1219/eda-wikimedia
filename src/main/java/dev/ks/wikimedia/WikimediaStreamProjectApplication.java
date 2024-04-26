d4c3faac3d572f4ba3f007edac8f325eb87a8d7d
Kai Sheng <76138041+kaisheng1219@users.noreply.github.com>
2024-04-26 17:09:13 +0800

    :hatching_chick: release: initial commit

---
+++ src/main/java/dev/ks/wikimedia/WikimediaStreamProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.ks.wikimedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaStreamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikimediaStreamProjectApplication.class, args);
+	}
+
+}
---
+++ wikimedia-producer/src/main/java/dev/ks/WikimediaProducerApplication.java
@@ -0,0 +1,12 @@
+package dev.ks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaProducerApplication {
+    public static void main( String[] args )
+    {
+        SpringApplication.run(WikimediaProducerApplication.class);
+    }
+}
