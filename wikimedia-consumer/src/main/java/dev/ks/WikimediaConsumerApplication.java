269406a8c04e3de80d85a0b6438cb083c1030495
Kai Sheng <76138041+kaisheng1219@users.noreply.github.com>
2024-04-27 15:58:20 +0800

    :heavy_plus_sign: update: add wikimedia consumer module

---
+++ wikimedia-consumer/src/main/java/dev/ks/WikimediaConsumerApplication.java
@@ -0,0 +1,11 @@
+package dev.ks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaConsumerApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(WikimediaConsumerApplication.class);
+    }
+}
